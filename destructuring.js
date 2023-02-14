abc312596311422fa475096744a44e6e5df799bf
robinozer <robin.ozer@outlook.com>
2023-02-14 16:26:57 +0000

    add notes on destructuring

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,38 @@
  * command node destructuring.js in the terminal
  */
 
-// Destructuring arrays
+// Destructuring arrays: instead of doing this:
+
+// let ages [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+// console.log(john, mary, joe);
+
+let ages = [30, 26, 27];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant"
+};
+let {mike, jill, alicia} = jobs
+console.log(mike, jill, alicia)
 
-// Destructuring subsets
+// Destructuring subsets (the rest of the parameters are ignored)
+// (leave commas  in the place of the values you want to skip)
+let languages = ["eng", "fre", "spa", "ger", "jap"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary)
 
-// Using rest parameter syntax
+// Using rest parameter syntax 
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite)
+console.log(secondFavorite)
+console.log(others)
+// here others becomes an array of the remaining elements
+
